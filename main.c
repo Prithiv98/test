bb0b76ef465ed4fe8932724cfe67f953aa8519be
Prithiv98 <57167697+Prithiv98@users.noreply.github.com>
2019-10-30 21:48:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int l, h, i, f;
+    printf("Enter two first number: ");
+    scanf("%d %d", &l);
+    printf("Enter the highest number");
+    scanf("%d", &h);
+    printf("Prime numbers between %d and %d are: ", l, h);
+    while (l < h)
+    {
+        f = 0;
+        for(i = 2; i <= l/2; ++i)
+        {
+            if(low % i == 0)
+            {
+                f = 1;
+                break;
+            }
+        }
+        if (f == 0)
+            printf("%d ", l);
+        ++l;
+    }
+    return
+}
